eb3582adac09aff176a60cd5001481becb5ed356
Simon Luijk <simon@simonluijk.com>
2015-07-28 17:35:46 +0200

    Pull changes from YoApp
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-docker-ebs-attach
-==============
+# ebs-attach
 
-Docker container that attaches an EBS volume to the local ec2 instance.
+Docker container that attaches an EBS volume to the local EC2 instance. Useful
+with CoreOS when you need persistent storage.
 
-Usage: 
+Usage:
 
 ```
-docker run -it --rm leg100/ebs-attach \
+docker run -it --rm justyo/ebs-attach \
 --volumeid vol-123123 \
 --device /dev/xvdf \
---region eu-west-1
+--region eu-west-1 \
+--access-key AWS_ACCESS_KEY \
+--secret-key AWS_SECRET_KEY
 ```
 
-Note: it relies on the ec2 instance possessing an IAM profile with the following privileges:
+Note: if you don't provide an IAM access and secret key, it relies on the EC2
+instance possessing an IAM profile with the following privileges:
 
- - ec2:AttachVolume
- - ec2:DetachVolume
+- ec2:AttachVolume
+- ec2:DetachVolume
+
+## Credits
+
+The wonderful [leg100](https://github.com/leg100) created the original version
+of ebs-attach.
--- ebs-attach.py
+++ ebs-attach.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import argparse
 import os
@@ -11,13 +11,16 @@ import boto.exception
 import requests
 
 # on close, detach ebs
-parser = argparse.ArgumentParser(description='Attach and mount EBS volume to local instance')
-parser.add_argument('--volumeid', metavar='<VOLUME_ID>', default=os.environ.get('VOLUME_ID'),
+parser = argparse.ArgumentParser(
+    description='Attach and mount EBS volume to local instance')
+parser.add_argument('--volumeid', metavar='<VOLUME_ID>', default=None,
                     help='Volume ID of EBS volume to attach')
-parser.add_argument('--device', metavar='<DEVICE>', default=os.environ.get('DEVICE'),
+parser.add_argument('--device', metavar='<DEVICE>', default=None,
                     help='Device to expose volume on')
-parser.add_argument('--region', metavar='<REGION>', default=os.environ.get('REGION'),
-                    help='AWS region')
+parser.add_argument('--region', metavar='<REGION>', default=None,
+                    help='AWS region in which the EBS resides')
+parser.add_argument('--access-key', metavar='<ACCESS>', default=None)
+parser.add_argument('--secret-key', metavar='<SECRET>', default=None)
 args = parser.parse_args()
 
 # AWS metadata service that provides credentials is unreliable
@@ -25,7 +28,9 @@ args = parser.parse_args()
 attempts = 0
 while True:
     try:
-        conn = boto.ec2.connect_to_region(args.region)
+        conn = boto.ec2.connect_to_region(args.region,
+                                          aws_access_key_id=args.access_key,
+                                          aws_secret_access_key=args.secret_key)
     except boto.exception.NoAuthHandlerFound:
         print "Couldn't find auth credentials handler, trying again"
         sys.stdout.flush()
@@ -56,6 +61,7 @@ else:
     print "Cannot find volume {}".format(args.volumeid)
     sys.exit(2)
 
+
 def detach_func(volume, instance, device):
     def handler(*args, **kwargs):
         attempts = 0
@@ -74,7 +80,7 @@ def detach_func(volume, instance, device):
                     sys.exit(3)
                 else:
                     time.sleep(1)
-           else:
+            else:
                 sys.exit(0)
     return handler
 
